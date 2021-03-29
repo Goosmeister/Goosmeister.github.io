a4fc84ff20cbf4751cf38be70bae0824f1b01e94
Gustav Kotze <edgegustavkotze@gmail.com>
2021-03-29 15:00:08 +0200

    const

--- script/script.js
+++ script/script.js
@@ -2,14 +2,15 @@
 window.addEventListener("scroll", () => {
       document.body.style.setProperty
       ("--scroll",window.pageYOffset / (document.body.offsetHeight - window.innerHeight));},false);
-  /* burgermenu*/
+ 
+/* burgermenu*/
 
 function openMenu() {
-      var b = document.getElementById("toggle");
-      var cross = document.getElementById("crossMain");
-      var burger = document.getElementById("burgerMain");
-      var cross1 = document.getElementById("cross1");
-      var cross2 = document.getElementById("cross2");
+      const b = document.getElementById("toggle");
+      const cross = document.getElementById("crossMain");
+      const burger = document.getElementById("burgerMain");
+      const cross1 = document.getElementById("cross1");
+      const cross2 = document.getElementById("cross2");
       if( toggle.checked == 0){
         cross.style.display = "block";
         burger.style.display = "none";
@@ -24,11 +25,11 @@ function openMenu() {
       
     }
 function closeMenu() {
-      var toggle = document.getElementById("toggle").checked = 1;
-      var cross = document.getElementById("crossMain");
-      var burger = document.getElementById("burgerMain");
-      var cross1 = document.getElementById("cross1");
-      var cross2 = document.getElementById("cross2");
+  const toggle = document.getElementById("toggle").checked = 1;
+  const cross = document.getElementById("crossMain");
+  const burger = document.getElementById("burgerMain");
+  const cross1 = document.getElementById("cross1");
+  const cross2 = document.getElementById("cross2");
       cross.style.display = "none";
       burger.style.display = "block";
       cross1.classList.remove("cross1");
